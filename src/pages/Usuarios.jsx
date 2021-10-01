cebd450a0ce38c7739e5e5ba1e9fdc66e5f11c4b
Vanessa <vane789vane@hotmail.com>
2021-10-01 17:27:25 -0500

    Agregar tablas de interfaz de usuario

--- src/pages/Usuarios.jsx
+++ src/pages/Usuarios.jsx
@@ -1,13 +1,79 @@
 import SectionMain from 'components/SectionMain'
 import React from 'react'
 import usuarios1 from 'media/usuarios1.png'
+import editar from 'media/editar.png'
+import eliminar from 'media/eliminar.png'
 
 const Usuarios = () => {
     return (
         <SectionMain nombre='usuarios' logo={usuarios1} >
-         Pagina de usuarios   
+          <TablaUsuarios/>
         </SectionMain>
     )
 }
+
+const TablaUsuarios = () => {
+    return (
+        <div className="flex flex-col items-center justify-center">
+        <table className=" border-separate bg-gray-400 w-3/4"> 
+            <thead>
+                <tr>
+                    
+                    <th className=" border-solid border-separate border border-gray-500 p-3">No.</th>
+                    <th className="border-separate border border-gray-500 p-3">Nombre usuario</th>
+                    <th className="border-separate border border-gray-500 p-3">Rol</th>
+                    <th className="border-separate border border-gray-500 p-3">Estado</th>
+                    <th className="border-separate border border-gray-500 p-3"></th>
+                </tr>
+            </thead>
+            <tbody className="bg-white">
+                <tr>
+                    <td>1</td>
+                    <td>Admin</td>
+                    <td>Administrador</td>
+                    <td>Autorizado</td>
+                    <td>
+                    <button>
+                    <img className='h-8 m-2 transform hover:scale-110' src={editar} alt="editar" />
+                    </button>
+                    <button>
+                    <img className='h-8 m-2 transform hover:scale-110' src={eliminar} alt="eliminar" />
+                    </button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Carol</td>
+                    <td>Vendedor</td>
+                    <td>Pendiente</td>
+                    <td>
+                    <button>
+                    <img className='h-8 m-2 transform hover:scale-110' src={editar} alt="editar" />
+                    </button>
+                    <button>
+                    <img className='h-8 m-2 transform hover:scale-110' src={eliminar} alt="eliminar" />
+                    </button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Vanessa</td>
+                    <td>Administrador</td>
+                    <td>No autorizado</td>
+                    <td>
+                    <button>
+                    <img className='h-8 m-2 transform hover:scale-110' src={editar} alt="editar" />
+                    </button>
+                    <button>
+                    <img className='h-8 m-2 transform hover:scale-110' src={eliminar} alt="eliminar" />
+                    </button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+      </div>
+    )
+}
+
 
 export default Usuarios
