bfd1626b6241a60411a8d6a581eb28ec91107c82
TATIANA2024 <78865126+TATIANA2024@users.noreply.github.com>
2021-10-01 13:45:13 -0500

    cambios ventas

--- src/pages/Ventas.jsx
+++ src/pages/Ventas.jsx
@@ -2,10 +2,35 @@ import React from 'react'
 
 const Ventas = () => {
     return (
-        <>
-         pagina de ventas   
-        </>
+       
+        <div>   
+         < TablaVentas/>
+        </div>
+   
+    
     )
 }
+
+
+const TablaVentas = () => {
+    return (
+        <div className="flex flex-col h-full w-full items-center ">
+        <table>
+         <tr>
+             <th># Identificación</th> 
+             <th>Descripción</th>
+             <th>Fecha Venta</th>
+             <th>Responsable</th>
+             <th>Estado</th>
+             <th>Valor Total</th>
+         </tr>
+        </table>
+    
+    </div>
+    )
+}
+
+
+
 
 export default Ventas
