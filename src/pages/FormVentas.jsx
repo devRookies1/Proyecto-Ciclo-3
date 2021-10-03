4ca29bfb4243451601d88f45560461e3f0572b70
Vanessa <vane789vane@hotmail.com>
2021-10-02 23:14:47 -0500

    Nuevos cambios formVentas

--- src/pages/FormVentas.jsx
+++ src/pages/FormVentas.jsx
@@ -5,74 +5,66 @@ import agregar from 'media/agregar.png'
 const FormVentas = () => {
     return (
         <SectionMainForm nombre='ventas'>
-            <div className="flex flex-row items-center justify-center space-x-20 p-2 ">
+            <div className="flex flex-col items-center justify-center p-5 space-y-8" >
               <form>
-                <table>
+                <table className=" border-separate bg-gray-400 "> 
+                    <thead>
+                        <tr>
+                        <th className="border-separate border border-gray-500 ">Identificador de venta</th>
+                        <th className="border-separate border border-gray-500 ">Nombre del cliente </th>
+                        <th className="border-separate border border-gray-500 ">Identificación del cliente</th>
+                        <th className="border-separate border border-gray-500 ">Fecha de la venta</th>
+                        <th className="border-separate border border-gray-500 ">Responsable</th>
+                        </tr>
+                    </thead>
+
+                <tbody className="bg-white">
                 <tr>
-                    <td><p>Identificador de venta:</p></td>
-                    <td><p><input type="text" placeholder = "Identificador de venta"/></p></td>
+                  <td><input type="text" placeholder = "Identificador de venta"/></td>
+                  <td><input type="text" placeholder ="Nombre del cliente"/></td>
+                  <td><input type="text" placeholder ="Identificación del cliente"/></td>
+                  <td><input type="date" placeholder ="Fecha de la venta"/></td>
+                  <td><input type="text" placeholder ="Responsable"/></td>
                 </tr>
-                <tr>
-                    <td><p>Nombre del cliente: </p></td>
-                    <td><p><input type="text" placeholder ="Nombre del cliente"/></p></td>
-                </tr>
-                <tr>
-                    <td><p>Identificación del cliente: </p></td>
-                    <td><p><input type="text" placeholder ="Identificación del cliente"/></p></td>
-                </tr>
-                <tr>
-                    <td><p>Fecha de la venta: </p></td>
-                    <td><p><input type="date" placeholder ="Fecha de la venta"/></p></td>
-                </tr>
-                <tr>
-                    <td><p>Responsable: </p></td>
-                    <td><p><input type="text" placeholder ="Responsable"/></p></td>
-                </tr>                      
+                </tbody>                        
             </table>
-        
           </form>
               <form>
-                <table>
-                <tr>
-                    <td><p>Producto:</p></td>
-                    <td><p><input type="text" placeholder = "Producto"/></p></td>
-                </tr>
-                <tr>
-                    <td><p>Descripción: </p></td>
-                    <td><p><input type="text" placeholder ="Descripcion"/></p></td>
-                </tr>
-                <tr>
-                    <td><p>Valor unitario: </p></td>
-                    <td><p><input type="text" placeholder ="Valor unitario"/></p></td>
-                </tr>
-                <tr>
-                    <td><p>Cantidad: </p></td>
-                    <td><p><input type="number" placeholder ="Cantidad"/></p></td>
-                </tr>
-                <tr>
-                    <td><p>Estado</p></td>                
-                    <td><p><select>
+                <table className="border-separate bg-gray-400"> 
+                    <thead>
+                        <tr>
+                        <th className="border-separate border border-gray-500 ">Producto</th>
+                        <th className="border-separate border border-gray-500 ">Descripción </th>
+                        <th className="border-separate border border-gray-500 ">Valor unitario</th>
+                        <th className="border-separate border border-gray-500 ">Cantidad</th>
+                        <th className="border-separate border border-gray-500 ">Estado</th>
+                        <th className="border-separate border border-gray-500 ">Total</th>
+                        </tr>
+                    </thead>
+                    <tbody className="bg-white">
+                    <tr>
+                        <td><input type="text" placeholder = "Producto"/></td>
+                        <td><input type="text" placeholder ="Descripcion"/></td>
+                        <td><input type="text" placeholder ="Valor unitario"/></td>
+                        <td><input type="number" placeholder ="Cantidad"/></td>                
+                        <td><p><select>
                         <option selected disabled>Estado</option>
                         <option value="Cancelado">Cancelado</option>
                         <option value="Entregado">Entregado</option>
                         <option value="En proceso">En proceso</option>
-                    </select></p></td>              
-                </tr>    
+                    </select></p></td>
+                        <td><input type="text" placeholder ="Total"/></td>
+                      </tr>
+                    </tbody>   
                 <tr>
-                    <td><p>Valor total: </p></td>
-                    <td><p><input type="number" placeholder ="Valor total"/></p></td>
                 </tr> 
-                <tr>
+            </table>
                 <button className>
-                    <h3 className="font-bold">Agregar otro producto</h3>
                     <img className='h-5 m-2 transform hover:scale-110' src={agregar} alt="agregar" />
                 </button>
-                </tr> 
-             
-            </table>
           </form>
           </div>
-          <div className=" flex items-center place-self-end">
+          <div className=" flex place-self-end">
           <table className="bg-gray-400 m-10 w-32 text-center">
             <h3 className="font-bold">Total venta</h3>
           <label htmlFor="Total venta"></label> Total Venta
