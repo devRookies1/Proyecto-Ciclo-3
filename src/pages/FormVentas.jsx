5ab5c518e2bedb71581695f3574fa5c628029dcf
TATIANA2024 <78865126+TATIANA2024@users.noreply.github.com>
2021-10-02 23:41:31 -0500

    cambio Ventas

--- src/pages/FormVentas.jsx
+++ src/pages/FormVentas.jsx
@@ -53,9 +53,50 @@ const FormVentas = () => {
 
     </form>
 
-
-      
-
+   
+           
+                        <div className="rp_subtitulo">INGRESE LA INFORMACION DEL PEDIDO</div>
+                        
+                        <div className="rp_formulario1">
+                            <form onSubmit={enviarDatos}>
+                                <table className="tabla">
+                                    <tr>
+                                        <td><p>Venta ID:</p></td>
+                                        <td><p><input className="input_m" type="text" id="venta_id" name="venta_id" size="30" onChange={entrada}/></p></td>
+                                    </tr>
+                                    <tr>
+                                        <td><p>Venta Total: </p></td>
+                                        <td><p><input className="input_m" type="text" id="venta_total" name="venta_total" size="30" onChange={entrada}/></p></td>
+                                    </tr>
+                                    <tr>
+                                        <td><p>Detalle: </p></td>
+                                        <td><p><input className="input_m" type="text" id="detalle" name="detalle" size="30" onChange={entrada}/></p></td>
+                                    </tr>
+                                    <tr>
+                                        <td><p>Fecha de Pago: </p></td>
+                                        <td><p><input className="input_m" type="date" id="fecha_de_pago" name="fecha_de_pago" size="30" onChange={entrada}/></p></td>
+                                    </tr>
+                                    <tr>
+                                        <td><p>Fecha de Pago Futura:&emsp;&emsp;</p></td>
+                                        <td><p><input className="input_m" type="date" id="fecha_de_pago_futura" name="fecha_de_pago_futura" size="30" onChange={entrada}/></p></td>
+                                    </tr>
+                                    <tr>
+                                        <td><p>Responsable: </p></td>
+                                        <td><p><input className="input_m" type="text" id="responsable" name="responsable" size="30" onChange={entrada}/></p></td>
+                                    </tr>
+                                    <tfoot>
+                                    <tr>
+                                        <td colSpan="2">
+                                            <button className="boton_m" type="submit">Ingresar Pedido</button>
+                                        </td>
+                                    </tr>
+                                        </tfoot>
+                                </table>
+                            </form>
+                        </div>
+                    
+             
+                        
 
         </SectionMainForm>
     )
