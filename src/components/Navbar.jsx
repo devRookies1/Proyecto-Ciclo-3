65cef495ce7f97efcc913e41fed92ed48c26e061
Paula Andrea Trejos <65204309+patrejos@users.noreply.github.com>
2021-10-25 14:25:46 -0500

    cambiar logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   const { user, logout } = useAuth0()
 
   const cerrarSesion = () => {
-    logout({ returnTo: 'http://localhost:3000'});
+    logout({ returnTo: 'https://boiling-crag-86589.herokuapp.com/'});
     localStorage.setItem('token', null);
   };
   
